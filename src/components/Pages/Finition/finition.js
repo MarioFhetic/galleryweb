062b7fc948c085f8450e4f478e57ee166ddd02f3
MarioFhetic <fayollemario@outlook.fr>
2020-05-20 14:25:27 +0200

    :sparkles: parallax

--- src/components/Pages/Finition/finition.js
+++ src/components/Pages/Finition/finition.js
@@ -1,205 +1,160 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./finition.css";
 
 import LogoFinition from "../Logos/logoFinition";
+import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons";
 
-const Finitionn = () => {
+const Finition = () => {
+  const parallax = useRef(null);
   return (
-    <div className={styles.bigContainer}>
-      <LogoFinition />
-      <div className={styles.containerHeader}>
-        <div className={styles.header}>
-          <h1>
-            Plusieurs solutions de finition vous sont proposées avant de
-            commander une image.
-          </h1>
-        </div>
-      </div>
-      <div className={styles.titleSection}>
-        <h2>CONTRE COLLÉ ET CHASSIS ALUMINIUM</h2>
-      </div>
-      <div className={styles.section}>
-        <div className={styles.leftContent}>
-          <p>
-            La photographie est contrecollée de façon permanente sur une plaque
-            rigide de Dibond™ de 3 mm d’épaisseur au format exact de l'image,
-            sur laquelle est collé au dos un châssis en aluminium de 2 cm
-            d'épaisseur, rentrant de 5 cm.
-          </p>
-          <div className={styles.containerImg}>
-            <img
-              src="../../images/finitions/section1left.png"
-              alt="Photographie faite en contre collé et chassis alumminium"
-            />
+    <Parallax ref={parallax} pages={6}>
+      <ParallaxLayer offset={0}>
+        <div className={styles.bigContainer}>
+          <LogoFinition />
+          <div className={styles.containerHeader}>
+            <div className={styles.header}>
+              <h1>
+                Plusieurs solutions de finition vous sont proposées avant de
+                commander une image.
+              </h1>
+            </div>
+          </div>
+          <div className={styles.titleSection}>
+            <h2>CONTRE COLLÉ ET CHASSIS ALUMINIUM</h2>
+          </div>
+          <div className={styles.section}>
+            <div className={styles.leftContent}>
+              <p>
+                La photographie est contrecollée de façon permanente sur une
+                plaque rigide de Dibond™ de 3 mm d’épaisseur au format exact de
+                l'image, sur laquelle est collé au dos un châssis en aluminium
+                de 2 cm d'épaisseur, rentrant de 5 cm.
+              </p>
+              <div className={styles.containerImg}>
+                <img
+                  src="../../images/finitions/section1left.png"
+                  alt="Photographie faite en contre collé et chassis alumminium"
+                />
+              </div>
+            </div>
+            <div className={styles.rightContent}>
+              <img
+                src="../../images/finitions/section1right.png"
+                alt="Photographie faite en contre collé et chassis alumminium"
+              />
+            </div>
+          </div>
+          <div className={styles.infoSection}>
+            <p>
+              Cette finition garantie une excellente rigidité et permet une
+              accroche décollée du mur.
+            </p>
           </div>
         </div>
-        <div className={styles.rightContent}>
-          <img
-            src="../../images/finitions/section1right.png"
-            alt="Photographie faite en contre collé et chassis alumminium"
-          />
-        </div>
-      </div>
-      <div className={styles.infoSection}>
-        <p>
-          Cette finition garantie une excellente rigidité et permet une accroche
-          décollée du mur.
-        </p>
-      </div>
-      <div className={styles.firstImgContainer}>
-        {/* <div className={styles.overlayImgContainer}></div> */}
-        {/* <div className={styles.infoBigImg}>
-          <p>contrecollage dibond et chassis</p>
-        </div> */}
-      </div>
-      <div className={styles.infoSectionBigImg}>
-        <p>CONTRECOLLÉ DIBOND ET CHASSIS</p>
-      </div>
-      <div className={styles.secondImgContainer}></div>
-      <div className={styles.infoSectionBigImg}>
-        <p>CONTRECOLLAGE ALUMINIUM ET CHASSIS</p>
-      </div>
-
-      {/* SECOND SECTION */}
-
-      <div className={styles.titleSection}>
-        <h2>CONTRECOLLAGE + CAISSE AMÉRICAINE</h2>
-      </div>
-      <div className={styles.section}>
-        <div className={styles.leftContentSecond}>
-          <p>
-            La photographie est contrecollée de façon permanente sur une plaque
-            rigide de Dibond™ de 3 mm d’épaisseur au format exact de l'image
-          </p>
-          <div className={styles.containerImgSecond}>
-            <img
-              src="../../images/finitions/firstimgsection2left.jpg"
-              alt="Photographie faite en contre collé et chassis alumminium"
-            />
+      </ParallaxLayer>
+      <ParallaxLayer offset={1} speed={0.8}>
+        <div className={styles.firstImgContainer}></div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={1.999} speed={1}>
+        <div className={styles.secondImgContainer}></div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={2} speed={1.5}>
+        <div className={styles.bigContainer}>
+          <div className={styles.titleSection}>
+            <h2>CONTRECOLLAGE + CAISSE AMÉRICAINE</h2>
           </div>
-          <p>
-            L’ensemble est intégré dans un cadre en bois naturel, noir ou blanc.
-            Largeur de la bordure du cadre, 9 mm, profondeur 35 mm.
-          </p>
+          <div className={styles.section}>
+            <div className={styles.leftContentSecond}>
+              <p>
+                La photographie est contrecollée de façon permanente sur une
+                plaque rigide de Dibond™ de 3 mm d’épaisseur au format exact de
+                l'image
+              </p>
+              <div className={styles.containerImgSecond}>
+                <img
+                  src="../../images/finitions/firstimgsection2left.jpg"
+                  alt="Photographie faite en contre collé et chassis alumminium"
+                />
+              </div>
+              <p>
+                L’ensemble est intégré dans un cadre en bois naturel, noir ou
+                blanc. Largeur de la bordure du cadre, 9 mm, profondeur 35 mm.
+              </p>
+            </div>
+            <div className={styles.rightContentSecond}>
+              <img
+                src="../../images/finitions/secondimgsection2right.png"
+                alt="Photographie faite en contre collé et chassis alumminium"
+              />
+            </div>
+          </div>
+          <div className={styles.infoSection}>
+            <p>
+              UN CADRE SANS VERRE, SOBRE ET ELEGANT QUI MET EN VALEUR LE GRAIN
+              DU PAPIER SANS AUCUN REFLET.
+            </p>
+          </div>
         </div>
-        <div className={styles.rightContentSecond}>
-          <img
-            src="../../images/finitions/secondimgsection2right.png"
-            alt="Photographie faite en contre collé et chassis alumminium"
-          />
+      </ParallaxLayer>
+      <ParallaxLayer offset={2.999} speed={2.1}>
+        <div className={styles.thirdImgContainer}></div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={2.999} speed={6}>
+        <div className={styles.fourthImgContainer}></div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={3.1} speed={0.01}>
+        <div className={styles.bigContainer}>
+          <div className={styles.titleSection}>
+            <h2>PLEXICOLLAGE</h2>
+          </div>
+          <div className={styles.containerPlexicollage}>
+            <div className={styles.leftSectionPlexicollage}>
+              <p>
+                Impression de l’image sur papier photo brillant Fuji 240g. Ce
+                papier restitue des noirs profonds et détaillés ainsi que des
+                blancs éclatants.
+              </p>
+              <p>
+                L’image est ensuite contrecollé sous un verre acrylique
+                transparent de 4 mm. L'adhérence est réalisée sans colle par la
+                réaction chimique entre deux composants liquides. Le système est
+                totalement dépourvu de poussière, de bulles d'air et de traces
+                de colle.
+              </p>
+              <p>
+                Une plaque d’Alu Dibond est appliquée au dos du tirage ainsi
+                qu’un châssis rentrant qui offre un excellente rigidité et une
+                accroche décollée du mur.
+              </p>
+              <p>
+                Une netteté d'image inégalée, le contraste des couleurs est
+                accentué et l'image acquiert une nouvelle profondeur sur toute
+                la surface et la protège à 99,7% contre les rayons UV.
+              </p>
+            </div>
+            <div className={styles.rightSectionPlexicollage}>
+              <img
+                src="../../images/finitions/firstimgthirdsection.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className={styles.infoSection}>
+            <p>
+              Le plexicollage donne aux images une brillance intense et un
+              effect de profondeur unique.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className={styles.infoSection}>
-        <p>
-          UN CADRE SANS VERRE, SOBRE ET ELEGANT QUI MET EN VALEUR LE GRAIN DU
-          PAPIER SANS AUCUN REFLET.
-        </p>
-      </div>
-      <div className={styles.thirdImgContainer}>
-        {/* <div className={styles.overlayImgContainer}></div> */}
-        {/* <div className={styles.infoBigImg}>
-          <p>contrecollage dibond et chassis</p>
-        </div> */}
-      </div>
-      <div className={styles.infoSectionBigImg}>
-        <p>CAISSE AMÉRICAINE ET CONTRECOLLÉ DIBOND</p>
-      </div>
-      <div className={styles.fourthImgContainer}></div>
-      {/* <div className={styles.infoSectionBigImg}>
-        <p>CONTRECOLLAGE ALUMINIUM ET CHASSIS</p>
-      </div> */}
-
-      {/* THIRD SECTION */}
-
-      <div className={styles.titleSection}>
-        <h2>PLEXICOLLAGE</h2>
-      </div>
-      <div className={styles.containerPlexicollage}>
-        <div className={styles.leftSectionPlexicollage}>
-          <p>
-            Impression de l’image sur papier photo brillant Fuji 240g. Ce papier
-            restitue des noirs profonds et détaillés ainsi que des blancs
-            éclatants.
-          </p>
-          <p>
-            L’image est ensuite contrecollé sous un verre acrylique transparent
-            de 4 mm. L'adhérence est réalisée sans colle par la réaction
-            chimique entre deux composants liquides. Le système est totalement
-            dépourvu de poussière, de bulles d'air et de traces de colle.
-          </p>
-          <p>
-            Une plaque d’Alu Dibond est appliquée au dos du tirage ainsi qu’un
-            châssis rentrant qui offre un excellente rigidité et une accroche
-            décollée du mur.
-          </p>
-          <p>
-            Une netteté d'image inégalée, le contraste des couleurs est accentué
-            et l'image acquiert une nouvelle profondeur sur toute la surface et
-            la protège à 99,7% contre les rayons UV.
-          </p>
-        </div>
-        <div className={styles.rightSectionPlexicollage}>
-          <img src="../../images/finitions/firstimgthirdsection.jpg" alt="" />
-        </div>
-      </div>
-      <div className={styles.infoSection}>
-        <p>
-          Le plexicollage donne aux images une brillance intense et un effect de
-          profondeur unique.
-        </p>
-      </div>
-      {/* <div className={styles.section}>
-        <div className={styles.leftContentThird}> */}
-      {/* <p>
-            Impression de l’image sur papier photo brillant Fuji 240g. Ce papier
-            restitue des noirs profonds et détaillés ainsi que des blancs
-            éclatants.
-          </p>
-          <p>
-            L’image est ensuite contrecollé sous un verre acrylique transparent
-            de 4 mm. L'adhérence est réalisée sans colle par la réaction
-            chimique entre deux composants liquides. Le système est totalement
-            dépourvu de poussière, de bulles d'air et de traces de colle.
-          </p>
-          <p>
-            Une plaque d’Alu Dibond est appliquée au dos du tirage ainsi qu’un
-            châssis rentrant qui offre un excellente rigidité et une accroche
-            décollée du mur.
-          </p>
-          <p>
-            Une netteté d'image inégalée, le contraste des couleurs est accentué
-            et l'image acquiert une nouvelle profondeur sur toute la surface et
-            la protège à 99,7% contre les rayons UV.
-          </p> */}
-      {/* </div> */}
-      {/* <div className={styles.rightContentThird}>
-          <img
-            src="../../images/finitions/firstimgthirdsection.jpg"
-            alt="Photographie faite en contre collé et chassis alumminium"
-          />
-        </div> */}
-      {/* </div> */}
-      {/* <div className={styles.infoSection}>
-        <p>
-          LE PLEXICOLLAGE DONNE AUX IMAGES UNE BRILLANCE INTENSE ET UN EFFET DE
-          PROFONDEUR UNIQUE.
-        </p>
-      </div> */}
-      <div className={styles.lastImgContainer}>
-        {/* <div className={styles.overlayImgContainer}></div> */}
-        {/* <div className={styles.infoBigImg}>
-          <p>contrecollage dibond et chassis</p>
-        </div> */}
-      </div>
-      <div className={styles.infoSectionBigImg}>
-        <p>PLEXICOLLAGE ET CONTRECOLLAGE SUR ALU DIBOND</p>
-      </div>
-      <div className={styles.secondLastImgContainer}></div>
-      <div className={styles.infoSectionBigImg}>
-        <p>OPTION CHASSIS EN BOIS DE 6 CM DE PROFONDEUR</p>
-      </div>
-    </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={4}>
+        <div className={styles.lastImgContainer}></div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={5}>
+        <div className={styles.secondLastImgContainer}></div>
+      </ParallaxLayer>
+    </Parallax>
   );
 };
 
-export default Finitionn;
+export default Finition;
--- src/components/Pages/Principe/principeBottom.js
+++ src/components/Pages/Principe/principeBottom.js
@@ -1,86 +1,90 @@
-import React, {useEffect } from "react";
-import styles from './principe.css';
-
+import React, { useEffect } from "react";
+import styles from "./principe.css";
 
 import { motion, useAnimation } from "framer-motion";
-import {useInView} from "react-intersection-observer";
+import { useInView } from "react-intersection-observer";
 
+const PrincipeBottom = () => {
+  const animation = useAnimation();
+  const [sectionBottomRef, inView] = useInView({
+    triggerOnce: true, // renvoi que une seule fois false puis que des true
+    rootMargin: "-150px",
+  });
 
-const PrincipeBottom = () => 
-{
+  useEffect(() => {
+    // si inView est set to true on run la variant "visible"
+    if (inView) animation.start("visible");
+  }, [animation, inView]); // on met une dépendance comme ça dès que inView est true ça trigger notre useEffect
 
-    const animation = useAnimation();
-    const [sectionBottomRef, inView] = useInView({
-        triggerOnce: true, // renvoi que une seule fois false puis que des true
-        rootMargin: '-300px' 
-
-    });
-
-    useEffect(() =>
-    {
-        // si inView est set to true on run la variant "visible"
-        if(inView)
-        (
-            animation.start('visible')
-        )
-
-    }, [animation, inView]) // on met une dépendance comme ça dès que inView est true ça trigger notre useEffect
-
-
-    return (
-        <motion.div 
-        className = {styles.sectionBottom}
-        ref = {sectionBottomRef}
-        animate = {animation}
-        initial = "hidden" // initial est set à hidden donc il sera caché avec un y de 72 à la base
-        variants = {{
-            visible: {
-                opacity: 1,
-                y: 0,
-                transition: {duration: 0.6, ease: [0.6,0.05,-0.01,0.9]}
+  return (
+    <motion.div
+      className={styles.sectionBottom}
+      ref={sectionBottomRef}
+      animate={animation}
+      initial="hidden" // initial est set à hidden donc il sera caché avec un y de 72 à la base
+      variants={{
+        visible: {
+          opacity: 1,
+          y: 0,
+          transition: { duration: 0.6, ease: [0.6, 0.05, -0.01, 0.9] },
         },
-            hidden: {
-                opacity: 0,
-                y: 72,
-            }
-        
-        }}
-        >
-            <div className = {styles.leftContent}>
-                <h2>EXPEDITION</h2>
-                <p>  
-                    Gratuite en France pour les tirages seuls
-                    La photo est enroulée dans une feuille de soie et emballée dans un tube de carton rigide puis expédiée à l’adresse de l’acquéreur ou à celle de son choix. 
-                </p>
+        hidden: {
+          opacity: 0,
+          y: 72,
+        },
+      }}
+    >
+      <div className={styles.leftContent}>
+        <h2>EXPEDITION</h2>
+        <p>
+          Gratuite en France pour les tirages seuls La photo est enroulée dans
+          une feuille de soie et emballée dans un tube de carton rigide puis
+          expédiée à l’adresse de l’acquéreur ou à celle de son choix.
+        </p>
 
-                <h2>FINITIONS</h2>
-                <p>  
-                    Plusieurs solutions d’encadrement vous sont proposées avant de commander une image.
-                </p>
+        <h2>FINITIONS</h2>
+        <p>
+          Plusieurs solutions d’encadrement vous sont proposées avant de
+          commander une image.
+        </p>
 
-                <p> 
-                    Caisse américaine en bois, finition noire, blanche ou naturelle. 
-                    Plexicollage.
-                </p>
-                <p>
-                    Le devis des finitions peut être défini par mail ou téléphone en fonction de votre choix, mais il est également possible de prendre rendez-vous pour une visite afin de constater la qualité des encadrements et des tirages sur les photographies disponibles à la galerie.
-                    La photographie commandée peut être mise à la disposition de l’acquéreur à l’atelier 154 rue Oberkampf 75011 Paris.
-                </p>
-
-            </div>
-            <div className = {styles.rightContent}>
-                <img src="../../images/principe/section2right.jpg" alt="Photographie d'un tableau en noir et blanc affiché au mur"/>
-            </div>
-            <div className = {styles.textEnd}>
-                <h2>COPYRIGHTS</h2>
-                <p>
-                    Comme toute création originale, une photographie originale porte l'empreinte de la personnalité de son auteur. Elle devient dès lors protégée par les articles L111-1 et suivants du Code français de la Propriété Intellectuelle comme œuvre de l'esprit bénéficiant du droit d'auteur. Les droits de l'acquéreur sur l'œuvre sont donc limités à un droit d'usage privé, excluant tout droit de représentation et de reproduction.
-                    © Tous droits réservés. Aucune image ne peut être utilisée sauf accord contractuel avec Eric Fayolle. Toute diffusion, publication et reproduction, numérique ou analogique, du contenu ou d´une partie du contenu du site Internet Photo Galerie Web Eric Fayolle doit être approuvée expressément par écrit par Eric Fayolle.
-                </p>
-            
-            </div>
-        </motion.div>
-    )
-}
+        <p>
+          Caisse américaine en bois, finition noire, blanche ou naturelle.
+          Plexicollage.
+        </p>
+        <p>
+          Le devis des finitions peut être défini par mail ou téléphone en
+          fonction de votre choix, mais il est également possible de prendre
+          rendez-vous pour une visite afin de constater la qualité des
+          encadrements et des tirages sur les photographies disponibles à la
+          galerie. La photographie commandée peut être mise à la disposition de
+          l’acquéreur à l’atelier 154 rue Oberkampf 75011 Paris.
+        </p>
+      </div>
+      <div className={styles.rightContent}>
+        <img
+          src="../../images/principe/section2right.jpg"
+          alt="Photographie d'un tableau en noir et blanc affiché au mur"
+        />
+      </div>
+      <div className={styles.textEnd}>
+        <h2>COPYRIGHTS</h2>
+        <p>
+          Comme toute création originale, une photographie originale porte
+          l'empreinte de la personnalité de son auteur. Elle devient dès lors
+          protégée par les articles L111-1 et suivants du Code français de la
+          Propriété Intellectuelle comme œuvre de l'esprit bénéficiant du droit
+          d'auteur. Les droits de l'acquéreur sur l'œuvre sont donc limités à un
+          droit d'usage privé, excluant tout droit de représentation et de
+          reproduction. © Tous droits réservés. Aucune image ne peut être
+          utilisée sauf accord contractuel avec Eric Fayolle. Toute diffusion,
+          publication et reproduction, numérique ou analogique, du contenu ou
+          d´une partie du contenu du site Internet Photo Galerie Web Eric
+          Fayolle doit être approuvée expressément par écrit par Eric Fayolle.
+        </p>
+      </div>
+    </motion.div>
+  );
+};
 
 export default PrincipeBottom;
